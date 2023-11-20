7ec2d863b6bde6a2504080aa52e6c8b934908239
J.H. Moon <moonjh587@naver.com>
2023-11-20 17:23:54 +0900

    Initial Commit

---
+++ MyWallet_SwiftUI/MyWallet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWallet_SwiftUIApp.swift
+//  MyWallet_SwiftUI
+//
+//  Created by 문정호 on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWallet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
